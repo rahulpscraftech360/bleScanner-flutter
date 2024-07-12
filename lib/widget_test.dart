6b7f616108bc905214e94b63db9588dcf6e1d970
rahulpscraftech360 <rahul@craftech360.com>
2024-07-12 15:45:44 +0530

    data is reading

--- lib/widget_test.dart
+++ lib/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:golfmy/main.dart';
+import 'package:golfmy/main1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
